522b15c53fdb2662461f64f9bcbda5c6be03b14e
solomonfy <fantahunsolomon@gmail.com>
2020-10-09 13:00:56 -0400

    user info

--- eth-connect-frontend/src/Components/Accounts.js
+++ eth-connect-frontend/src/Components/Accounts.js
@@ -1,37 +1,7 @@
-// import { useOktaAuth } from "@okta/okta-react";
-// import React, { useState, useEffect } from "react";
+import React from "react";
 
-// const Accounts = (props) => {
-//   const { authState, authService } = useOktaAuth();
-//   const [userInfo, setUserInfo] = useState(null);
+const Accounts = () => {
+  return <div>Welcome back</div>;
+};
 
-//   useEffect(() => {
-//     fetch(props.membersUrl + "member", {
-//       method: "GET",
-//       headers: {
-//         Authorization: `Bearer ${localStorage.token}`,
-//       },
-//     });
-//     if (!authState.isAuthenticated) {
-//       // When user isn't authenticated, forget any user info
-//       setUserInfo(null);
-//     } else {
-//       authService.getUser().then((info) => {
-//         setUserInfo(info);
-//       });
-//     }
-//   }, [authState, authService]); // Update if authState changes
-
-//   console.log(userInfo);
-//   return (
-//     <div>
-//       {userInfo && (
-//         <div>
-//           <p>Welcome back, {userInfo.name}!</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Accounts;
+export default Accounts;
