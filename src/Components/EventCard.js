288f1dbb46fd8216ed7d83279274351c79767922
solomonfy <fantahunsolomon@gmail.com>
2020-10-20 03:34:30 -0400

    editing event is done

--- src/Components/EventCard.js
+++ src/Components/EventCard.js
@@ -13,6 +13,7 @@ import {
 import ReviewForm from "./Forms/ReviewForm";
 
 const EventCard = (props) => {
+  let e_vent = props.anEvent;
   const {
     name,
     description,
@@ -45,6 +46,7 @@ const EventCard = (props) => {
           ui={false}
         /> */}
         <img src={event_card} height={200} />
+
         <Card.Content>
           <Card.Header>{name}</Card.Header>
           <Card.Meta>
@@ -109,9 +111,16 @@ const EventCard = (props) => {
                   <Link to="/new_invitation">
                     <Button primary>Invite members</Button>
                   </Link>
-                  <Button primary onClick={null}>
-                    Edit Event
-                  </Button>
+                  <Link
+                    to={{
+                      pathname: "edit-event",
+                      eventToBeEdited: props.anEvent,
+                    }}
+                  >
+                    <Button primary onClick={null}>
+                      Edit Event
+                    </Button>
+                  </Link>
                   <Button
                     negative
                     onClick={() => props.deleteEvent(props.anEvent)}
