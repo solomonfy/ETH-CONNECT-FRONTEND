9ba33830ab41a8682d6c06716e1f9990a88b29f6
solomonfy <fantahunsolomon@gmail.com>
2020-10-21 10:53:57 -0400

    sort

--- src/Components/Sort.js
+++ src/Components/Sort.js
@@ -7,13 +7,13 @@ const Sort = (props) => {
       {/* <div> */}
       <Label size="large">Sort events by:</Label>
       <select onChange={(e) => props.sortEvents(e.target.value)}>
-        <option value="all">All</option>
+        <option value="all">All - ሁሉም</option>
         <option value="upcoming" defaultValue>
-          Upcoming events
+          Upcoming events - የሚመጡ ክስተቶች
         </option>
-        <option value="past">Past events</option>
-        <option value="name">Name</option>
-        <option value="my_events">My Events</option>
+        <option value="past">Past events - ያለፉ ክስተቶች</option>
+        <option value="name">Name - የክስተቶች ስም</option>
+        <option value="my_events">My Events - የእኔ ክስተቶች</option>
       </select>
       {/* </div> */}
     </div>
